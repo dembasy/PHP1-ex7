9970b8141a812cfadcbdf2f869b345605c6f37a3
dembasy <dembasy1227@gmail.com>
2018-07-25 16:23:17 +0200

    done

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8" />
-    <title>ex5</title>
+    <title>ex7</title>
   </head>
   <body>
     <p><?php $lastName = "Demba";  $firstName = "Sylla"; $age = 23; echo 'Bonjour '.$lastName. ' '.$firstName. ' '. 'tu as '. $age. 'ans.' ; ?></p>
