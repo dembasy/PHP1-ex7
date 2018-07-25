d56f098c1baaf86588d22f0d14bd6c02867904d8
dembasy <dembasy1227@gmail.com>
2018-07-25 16:18:04 +0200

    done

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex5</title>
+  </head>
+  <body>
+    <p><?php $lastName = "Demba";  $firstName = "Sylla"; $age = 23; echo 'Bonjour '.$lastName. ' '.$firstName. ' '. 'tu as '. $age. 'ans.' ; ?></p>
+  </body>
+</html>
